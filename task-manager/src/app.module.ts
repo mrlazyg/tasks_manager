ff5505bb3d6cab44e522c7524d5784e713cb0299
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2023-06-01 02:59:50 +0530

    new: db connection

--- task-manager/src/app.module.ts
+++ task-manager/src/app.module.ts
@@ -2,8 +2,22 @@ import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_DB_HOST,
+      port: +process.env.PG_DB_PORT,
+      username: process.env.PG_DB_USER,
+      password: process.env.PG_DB_PASS,
+      database: process.env.PG_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+    }),
+  ],
 })
 export class AppModule {}
