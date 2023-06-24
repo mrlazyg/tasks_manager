51f9c95c492233d45eb8e9e371b2407536f0d233
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2023-06-25 00:27:54 +0530

    node engine

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import 'dotenv/config';
 
 @Module({
   imports: [
